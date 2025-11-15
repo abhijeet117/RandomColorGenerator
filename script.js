4535f0d924115be50f82ec9b7613d778d97554db
abhijeet117 <abhijeetkumarsah92@gmail.com>
2025-11-15 11:29:12 +0530

    added color code

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-let circle = document.querySelector('.circle');
+let circle = document.querySelector(".circle");
 
-let original = document.querySelector('.btn1');
-let change = document.querySelector('.btn2');
+let original = document.querySelector(".btn1");
+let change = document.querySelector(".btn2");
 
+let hex = document.querySelector(".hexid");
 
+change.addEventListener("click", function () {
+  let random1 = Math.floor(Math.random() * 256);
+  let random2 = Math.floor(Math.random() * 256);
+  let random3 = Math.floor(Math.random() * 256);
+  circle.style.backgroundColor = `rgb(${random1}, ${random2}, ${random3})`;
 
-change.addEventListener('click', function(){
-    let random1 = Math.floor(Math.random()*256);
-    let random2 = Math.floor(Math.random()*256);
-    let random3 = Math.floor(Math.random()*256);
-    circle.style.backgroundColor = `rgb(${random1}, ${random2}, ${random3})`;
-    
-})
+  hex.innerHTML =
+    `#${random1.toString(16).padStart(2, "0")}` +
+    `${random2.toString(16).padStart(2, "0")}` +
+    `${random3.toString(16).padStart(2, "0")}`;
+});
 
-original.addEventListener('click', function() {
-     circle.style.backgroundColor = 'transparent';
-})
+original.addEventListener("click", function () {
+  circle.style.backgroundColor = "transparent";
+  hex.innerHTML = "#cbcbcb";
+});
